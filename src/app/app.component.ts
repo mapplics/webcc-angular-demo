bcd578e572714af5276376dce04f8accc3f459e9
Santiago Garcia <santiagogarcia97@outlook.com>
2023-04-15 00:55:48 -0300

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import webccCreator from '../plugins/webcc';
-import * as webcc from "webcc";
+import * as webcclib from 'webcc';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +9,30 @@ import * as webcc from "webcc";
 })
 export class AppComponent {
   title = 'angular-demo';
+
+  webcc: any;
+
+  ngAfterViewInit() {
+    this.webcc = new webcclib.Runtime('eyJuYW1lIjoiUmhpbm8iLCJzdG0iOjE2Njc0MzM2MDAwMDAsImV4cCI6MTY5OTA1NjAwMDAwMCwibGl0ZSI6ZmFsc2V9.f923c735ceba1c42e678b3586ea37fde');
+  }
+
+  onToolChange(tool: string) {
+    this.webcc!.toolManager.takeTool(tool);
+  }
+
+  clear() {
+    this.webcc!.shapeManager.clear(true);
+  }
+
+  undo() {
+    this.webcc!.mometoManager.undo();
+  }
+
+  redo() {
+    this.webcc!.mometoManager.redo();
+  }
+
+  delete() {
+    this.webcc!.shapeManager.remove();
+  }
 }
--- src/plugins/webcc.js
+++
@@ -1,7 +0,0 @@
-import * as webcc from 'webcc'
-
-export default class webccCreator {
-    static create(){
-      return new webcc.Runtime('eyJuYW1lIjoiUmhpbm8iLCJzdG0iOjE2Njc0MzM2MDAwMDAsImV4cCI6MTY5OTA1NjAwMDAwMCwibGl0ZSI6ZmFsc2V9.f923c735ceba1c42e678b3586ea37fde');
-    }
-}
